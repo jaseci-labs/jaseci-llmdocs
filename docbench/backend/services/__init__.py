27db58495b356fee1710751e8575fdd11b086ebd
kevinjin420 <kevinjin0420@gmail.com>
2025-12-23 21:54:54 -0800

    separated benchmarking tool into own repo

--- docbench/backend/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backend services for evaluation and LLM interaction"""
-
-from .evaluator import EvaluatorService
-from .llm_service import LLMService
-from .graph_service import GraphService
-
-__all__ = ['EvaluatorService', 'LLMService', 'GraphService']
